1741e361285e14d26bacf01cece85d75c6b6efac
Mike Hsu <fatooo0116@gmail.com>
2015-07-28 18:14:26 +0800

    1
    1

--- footer.php
+++ footer.php
@@ -1,3 +1,45 @@
+
+
+<!-- page-section : starts -->
+<footer id="mastfoot" class="mastfoot">
+    <div class="clearfix" style="background: #2f3133">
+    </div>
+
+    <!-- inner-section : starts -->
+    <section class="inner-section footer-bottom">
+
+        <!-- container : starts -->
+        <section class="container">
+
+            <div class="row">
+                <article class="col-md-6 text-left">
+                    <h3>@twitter</h3>
+                    <article id="ticker" class="tweets-ticker query"></article>
+                </article>
+                <article class="col-md-6 text-right">
+                    <ul class="footer-social">
+                        <li><a href="mailto:mail@domain.tld" target="_blank"><img alt="誠品行旅" src="http://new.eslitehotel.com/skin/images/social_icons/email.png"></a></li>
+                        <li><a href="designova" target="_blank"><img alt="誠品行旅" src="http://new.eslitehotel.com/skin/images/social_icons/facebook.png"></a></li>
+                        <li><a href="designova" target="_blank"><img alt="誠品行旅" src="http://new.eslitehotel.com/skin/images/social_icons/google.png"></a></li>
+                        <li><a href="#" target="_blank"><img alt="誠品行旅" src="http://new.eslitehotel.com/skin/images/social_icons/vimeo.png"></a></li>
+
+                    </ul>
+                    <div class="credits">
+                        <p> <br>© 2015 All rights reserved.</p>
+                    </div>
+                </article>
+            </div>
+
+        </section>
+
+    </section>
+    <!-- inner-section : ends -->
+
+
+</footer>
+<!-- page-section : ends -->
+
+</section>
 <script src="http://localhost:35729/livereload.js"></script>
 <script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
